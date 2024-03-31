50f45f253d5297bcfcf868cc118e732384361421
WashimBar <washimbari0001@gmail.com>
2024-03-31 19:55:18 +0530

    code update

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./src/connection/connectDB");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
 const PORT = process.env.PORT
 const app = express();
 
+//request parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 app.get('/', (req, res) => { 
     res.sendFile(__dirname + '/index.html');
 })
+
+//set view engine
+app.set('view engine', 'ejs');
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));  
+
+//parser cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//set up routes
+
+
+//error handlers
+
 
 const start = async () => { 
     try {
@@ -19,4 +40,5 @@ const start = async () => {
         console.log(error.message)
     }
 }
+
 start();
